5badc8798833549f099413c95063adc1e09fc03b
clicktravel-charlotteneill <charlie_neill@hotmail.com>
2019-07-22 15:27:06 +0100

    updated calculator

--- calculator.py
+++ calculator.py
@@ -1,48 +1,29 @@
-# prompt the user for first number for Addition
-print("Enter first number please for Addition")
+#prompt the user to select the operator
+print("please choose the operator from 1-4")
+print("1 == +")
+print("2 == -")
+print("3 == *")
+print("4 == /")
+
+#promt the user to choose the operator
+operator = input() 
+
+# prompt the user for first number for choosen integer
+print("Enter first number please for choosen integer")
 firstNumber = input()
 
 # prompt the user for second number for Addition
-print("Enter the second number for Addition")
+print("Please enter the second number")
 secondNumber= input()
 
-#promt for the user for addition
-print(firstNumber+secondNumber)
+if (operator == 1):
+  print(firstNumber+secondNumber)
 
-#prompt the user for first multiplication number
-print(" Please enter your Firstnumber for multiplication")
-Firstnumber = input()
+elif (operator == 2):
+  print(firstNumber-secondNumber)
 
-#prompt the user for second multiplication number
-print(" Please enter your Secondnumber for multiplication")
-Secondnumber = input()
-
-#promt for the user for multiplication
-print(Firstnumber*Secondnumber)
-
-#prompt the user for first subtraction 
-print(" Please enter your Firstnumber for substraction")
-Firstnumber = input()
-
-#prompt the user for second substraction
-print(" Please enter your Secondnumber for substraction")
-Secondnumber = input()
-
-#promt for the user for division
-print(Firstnumber-Secondnumber)
-
-#prompt the user for first division
-print(" Please enter your Firstnumber for division")
-Firstnumber = input()
-
-#prompt the user for second division
-print(" Please enter your Secondnumber for division")
-Secondnumber = input()
-
-#promt for the user for division
-print(Firstnumber/Secondnumber)
-
-#
-# prompt the user for the operator (*,/,+,-)
-# prompt the user for second number
-# perform operations of first and second number, using the operator
+elif (operator == 3):
+  print(firstNumber*secondNumber)
+  
+elif (operator == 4):
+  print(firstNumber/secondNumber)
